54dc11f14271b81968af9ce19f0fc20c0c089eaf
epicjung <epicjung@github.com>
2021-09-09 17:20:35 +0900

    initial commit

---
+++ receive_gps/src/receive_gps.h
@@ -0,0 +1,41 @@
+#ifndef RECEIVE_GPS_H
+#define RECEIVE_GPS_H
+
+#include <ros/ros.h>
+#include <sensor_msgs/NavSatFix.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Transform.h>
+#include <nav_msgs/Path.h>
+#include <nav_msgs/Odometry.h>
+#include <iostream>
+#include <fstream>
+#include "conversions.h"
+#include <tf/transform_broadcaster.h>
+
+using namespace std;
+using namespace gps_common;
+
+#define PI 3.141592
+
+class GPS
+{
+public:
+	GPS();
+	~GPS();
+
+private:
+	ros::NodeHandle nh;
+	ros::Subscriber navsatfix_sub;
+	ros::Publisher pose_pub;
+	ros::Publisher gt_traj_pub;
+	ros::Publisher odom_pub;
+	bool first_data_flag;
+    double northing_offset, easting_offset;
+	nav_msgs::Path path;
+
+
+	void navSatFixCallback(const sensor_msgs::NavSatFixPtr& fix_msg);
+
+};
+
+#endif
